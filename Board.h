d269eb0b6d8ef12a0a42d5b6b7a82a3d45fe9058
Aram Hakobyan <aramhakobyanmail@gmail.com>
2022-09-01 21:30:34 +0400

    Bosrd.h,Figur.h,Piece.h

--- Board.h
+++ Board.h
@@ -7,4 +7,10 @@ class Board
 public:
 	std::vector<Figur*> figur[8];
 	bool game_color;
+	Board() {
+		figur[0].push_back(new Rook());
+		figur[0][0]->coord = { 0,0 };
+		figur[0][0]->piece = { Color::White,PieceName::Knight };
+	}
+	~Board(){}
 };
